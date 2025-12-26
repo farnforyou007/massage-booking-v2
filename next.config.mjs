fafb904601f7775679885a687b7b22aa8eb2a0da
farnforyou <6710210474@psu.ac.th>
2025-12-26 09:25:46 +0700

    docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   output: "standalone",
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   reactCompiler: true,
+//   images: {
+//     domains: ['profile.line-scdn.net'],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: "standalone",
+  output: "standalone", // ✅ 1. เพิ่มบรรทัดนี้ (สำคัญมากสำหรับ Docker)
   reactCompiler: true,
   images: {
     domains: ['profile.line-scdn.net'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ 2. เพิ่มบรรทัดนี้ (เพื่อข้ามการตรวจ Error เล็กน้อยตอน Build)
   },
 };
 
